6d5c21defbc41c73a85d84416b38130bbeb3f2b6
dmckenna44 <dmckenna05@gmail.com>
2023-01-10 12:34:36 -0800

    added middleware for logging in, signing up and verifying session
    Co-authored-by: Sam G <sammyb1rd@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
 const path = require('path');
-require('dotenv').config();
+require("dotenv").config();
 
 // Express Config
 const app = express();
@@ -10,11 +11,32 @@ express.static(path.resolve(__dirname, '../public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser()) // Need to pass in a cookie secret
+//session middleware
+app.use(sessions({
+  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+  saveUninitialized:true,
+  cookie: { maxAge: 86400000 },
+  resave: false
+}));
+
 
 ////////////////////////////////////////////////////////////////
 // ----------------------- MIDDLEWARE ----------------------- //
 ////////////////////////////////////////////////////////////////
 const recipeController = require('./controllers/recipeController');
+const userController = require('./controllers/userController');
+
+const URI = process.env.MONGO_URI;
+
+// DB connection
+mongoose.connect(URI, {dbName: 'dining-in'})
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch((err) => {
+    console.log(`there was an error connecting to mongodb: ${err}`)
+  })
+
 
 ////////////////////////////////////////////////////////////////
 // ----------------------- ENDPOINTS ------------------------ //
@@ -22,14 +44,30 @@ const recipeController = require('./controllers/recipeController');
 
 // ----------------------- User Endpoints ------------------------ //
 
+// Check session (home page)
+app.get('/home', userController.isLoggedIn, (req, res) => {
+  // res.status(200).send(res.locals.permission)
+  res.sendFile('../client/public/index.html')
+});
+
 // Login
-app.post('/api/login', (req, res) => {
-  res.status(200).send('Logged in!')
+app.post('/api/login', userController.verifyUser, (req, res) => {
+  console.log(res.locals.currentUser);
+  res.status(200).send(res.locals.currentUser);
 })
+
 // Signup
-app.post('/api/signup', (req, res) => {
-  res.status(200).send('New user signed up!');
+app.post('/api/signup', userController.addUser, (req, res) => {
+  res.status(200).json(res.locals.newUser);
 })
+
+
+
+//////sam add
+
+
+
+////sam end
 
 //------------------------------------------------------------------ //
 
@@ -63,6 +101,7 @@ app.use('/*', (req, res) => {
   res.status(404).send('404: Not Found');
 })
 
+// Global error handler
 app.use((err, req, res, next) => {
   const error = {
     errorBody: err.body,
