0c4eafee12e07f69442fa94fa689c14e6e0c51f6
dmckenna44 <dmckenna05@gmail.com>
2023-01-09 16:42:44 -0800

    added routes for signing up, logging in, adding and retrieving recipes, started middleware for recipes, still needs schemas

--- server/server.js
+++ server/server.js
@@ -20,14 +20,52 @@ app.use(cookieParser()) // Need to pass in a cookie secret
 ////////////////////////////////////////////////////////////////
 
 // Login
-
+app.post('/api/login', (req, res) => {
+  res.status(200).send('Logged in!')
+})
 // Signup
-
-// Get 
-app.get('/', (req, res) => {
-  res.send('Hello From the Back End!')
+app.post('/api/signup', (req, res) => {
+  res.status(200).send('New user signed up!');
 })
 
+// Get all recipes
+app.get('/api/recipes', (req, res) => {
+  res.status(200).json(res.locals.recipes)
+})
+
+// Get one (random) recipe
+app.get('/api/random', (req, res) => {
+  res.status(200).json(res.locals.random)
+})
+
+// Add a new recipe
+app.post('/api/addRecipe', (req, res) => {
+  res.status(200).send('New recipe added!');
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello From the Back End!');
+})
+
+
+// ----------------------- Global Endpoints ------------------------ //
+app.get('/*', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../build/index.html'));
+})
+
+app.use('/*', (req, res) => {
+  console.log(`Unrecognized route requested: ${req.method} request to ${req.url}`);
+  res.status(404).send('404: Not Found');
+})
+
+app.use((err, req, res, next) => {
+  const error = {
+    errorBody: err.body,
+    message: err.msg
+  }
+  const errMsg = `${err.msg} \n ${err.body}`
+  res.status(500).send(errMsg)
+})
 
 
 app.listen(3000, () => console.log('Server running on port 3000'));
