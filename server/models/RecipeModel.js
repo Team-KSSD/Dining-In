4bb3137d99655a68ce833d8ab5d235e14e9dd3ef
Samantha <samanthawarrick@gmail.com>
2023-01-09 22:45:29 -0500

    created document models and connected mongodb

---
+++ server/models/RecipeModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Schema = mongoose.Schema;
+const URI = process.env.MONGO_URI;
+
+mongoose.connect(URI, {dbName: 'dining-in'})
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch((err) => {
+    console.log(`there was an error connecting to mongodb: ${err}`)
+  })
+
+const recipeSchema = new Schema({
+  
+  cookTime: {
+    type: String, 
+    required: true
+  },
+  filters: [String],
+  difficulty: {
+    type: String,
+    required: true
+  },
+  ingredients: [{
+    item: {
+      type: String,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    unit: {
+      type: String, 
+      required: true
+    }
+  }],
+  steps: [{
+    type: String,
+    required: true
+  }]
+})
+
+const Recipes = mongoose.model('recipes', recipeSchema);
+module.exports = Recipes;
+
+// db.collection.updateOne(
+//     { _id: 5f2324a89b9c9b7d2bce1a4d },
+//     { $push: { tags: { name: "new_tag", color: "red" } } }
+//  )
---
+++ server/models/UserModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Schema = mongoose.Schema;
+const URI = process.env.MONGO_URI;
+
+mongoose.connect(URI, {dbName: 'dining-in'})
+  .then(() => {
+    console.log('connected to moongodb')
+  })
+  .catch((err) => {
+    console.log(`there was an error connecting to mongodb: ${err}`)
+  })
+
+  const userSchema = new Schema({
+    username: {
+      type: String, 
+      required: true
+    },
+    password: {
+      type: String, 
+      required: true
+    }
+  })
+
+const Users = mongoose.model('users', userSchema)
+module.exports = Users;
+
+// db.collection.updateOne(
+//     { _id: 5f2324a89b9c9b7d2bce1a4d },
+//     { $push: { tags: { name: "new_tag", color: "red" } } }
+//  )
